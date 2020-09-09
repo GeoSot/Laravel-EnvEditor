418455b0320fa4f3c9a4c7db37cae7d93b50c06b
GeoSot <geo.sotis@gmail.com>
2020-09-09 15:55:39 +0300

    add doc title

--- src/resources/views/layout.blade.php
+++ src/resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@lang('env-editor::env-editor.menuTitle')</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
