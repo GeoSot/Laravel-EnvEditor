d4443669b91571f6084154f68710eba3ac8d0b0a
GeoSot <geo.sotis@gmail.com>
2024-04-13 14:52:54 +0300

    ref: disable routes by default

--- routes/routes.php
+++ routes/routes.php
@@ -9,28 +9,28 @@ if (config('env-editor.route.enable')) {
     Route::prefix(config('env-editor.route.prefix'))
         ->middleware(config('env-editor.route.middleware'))
         ->group(function () use ($routeMainName) {
-            Route::get('/', [EnvController::class, 'index'])->name($routeMainName . '.index');
+            Route::get('/', [EnvController::class, 'index'])->name($routeMainName.'.index');
 
-            Route::post('key', [EnvController::class, 'addKey'])->name($routeMainName . '.key');
+            Route::post('key', [EnvController::class, 'addKey'])->name($routeMainName.'.key');
             Route::patch('key', [EnvController::class, 'editKey']);
             Route::delete('key', [EnvController::class, 'deleteKey']);
 
-            Route::delete('clear-cache', [EnvController::class, 'clearConfigCache'])->name($routeMainName . '.clearConfigCache');
+            Route::delete('clear-cache', [EnvController::class, 'clearConfigCache'])->name($routeMainName.'.clearConfigCache');
 
             Route::prefix('files')->group(function () use ($routeMainName) {
                 Route::get('/', [EnvController::class, 'getBackupFiles'])
-                    ->name($routeMainName . '.getBackups');
+                    ->name($routeMainName.'.getBackups');
                 Route::post('create-backup', [EnvController::class, 'createBackup'])
-                    ->name($routeMainName . '.createBackup');
+                    ->name($routeMainName.'.createBackup');
                 Route::post('restore-backup/{filename?}', [EnvController::class, 'restoreBackup'])
-                    ->name($routeMainName . '.restoreBackup');
+                    ->name($routeMainName.'.restoreBackup');
                 Route::delete('destroy-backup/{filename?}', [EnvController::class, 'destroyBackup'])
-                    ->name($routeMainName . '.destroyBackup');
+                    ->name($routeMainName.'.destroyBackup');
 
                 Route::get('download/{filename?}', [EnvController::class, 'download'])
-                    ->name($routeMainName . '.download');
+                    ->name($routeMainName.'.download');
                 Route::post('upload', [EnvController::class, 'upload'])
-                    ->name($routeMainName . '.upload');
+                    ->name($routeMainName.'.upload');
             });
         });
 }
